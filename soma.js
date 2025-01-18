c67274ecbcb743b88fe3a7e311176f650aa2806c
MinhaLoja <152699267+MeuSiteAki@users.noreply.github.com>
2025-01-18 09:58:14 -0300

    Add files via upload

---
+++ soma.js
@@ -0,0 +1,136 @@
+function somar() {
+                let A11 = document.getElementById("A11").value
+                let A12 = document.getElementById("A12").value
+                let A13 = document.getElementById("A13").value
+                let A14 = document.getElementById("A14").value
+                let A15 = document.getElementById("A15").value
+                let A16 = document.getElementById("A16").value
+                let A17 = document.getElementById("A17").value
+                let A18 = document.getElementById("A18").value
+                let A19 = document.getElementById("A19").value
+                let A20 = document.getElementById("A20").value
+                let A21 = document.getElementById("A21").value
+                let A22 = document.getElementById("A22").value
+                let A23 = document.getElementById("A23").value
+                let A24 = document.getElementById("A24").value
+                let A25 = document.getElementById("A25").value
+                let A26 = document.getElementById("A26").value
+                let A27 = document.getElementById("A27").value
+                let A28 = document.getElementById("A28").value
+                let A29 = document.getElementById("A29").value
+                let A30 = document.getElementById("A30").value
+                let A31 = document.getElementById("A31").value
+                let A32 = document.getElementById("A32").value
+                let A33 = document.getElementById("A33").value
+                let A34 = document.getElementById("A34").value
+                let A35 = document.getElementById("A35").value
+                let A36 = document.getElementById("A36").value
+                let A37 = document.getElementById("A37").value
+                let A38 = document.getElementById("A38").value
+                let A39 = document.getElementById("A39").value
+                let A40 = document.getElementById("A40").value
+                
+
+
+     if (!isNaN(A11) && !isNaN(A12) && !isNaN(A13) && !isNaN(A14) && !isNaN(A15) && !isNaN(A16) && !isNaN(A17) && !isNaN(A18) && !isNaN(A19) && !isNaN(A20)
+         && !isNaN(A21) && !isNaN(A22) && !isNaN(A23) && !isNaN(A24) && !isNaN(A25) && !isNaN(A26) && !isNaN(A27) && !isNaN(A28) && !isNaN(A29) && !isNaN(A30)
+         && !isNaN(A31) && !isNaN(A32) && !isNaN(A33) && !isNaN(A34) && !isNaN(A35) && !isNaN(A36) && !isNaN(A37) && !isNaN(A38) && !isNaN(A39) && !isNaN(A40)
+         ){
+
+             let resultado = Number(A11) + Number(A12) + Number(A13) + Number(A14) + Number(A15) + Number(A16) + Number(A17) + Number(A18) + Number(A19) + Number(A20)
+                + Number(A21) + Number(A22) + Number(A23) + Number(A24) + Number(A25) + Number(A26) + Number(A27) + Number(A28) + Number(A29) + Number(A30)
+                 + Number(A31) + Number(A32) + Number(A33) + Number(A34) + Number(A35) + Number(A36) + Number(A37) + Number(A38) + Number(A39) + Number(A40)
+                 
+                 alert(resultado)
+                }
+         }
+         function somar2() {
+            let B11 = document.getElementById("B11").value
+            let B12 = document.getElementById("B12").value
+            let B13 = document.getElementById("B13").value
+            let B14 = document.getElementById("B14").value
+            let B15 = document.getElementById("B15").value
+            let B16 = document.getElementById("B16").value
+            let B17 = document.getElementById("B17").value
+            let B18 = document.getElementById("B18").value
+            let B19 = document.getElementById("B19").value
+            let B20 = document.getElementById("B20").value
+            let B21 = document.getElementById("B21").value
+            let B22 = document.getElementById("B22").value
+            let B23 = document.getElementById("B23").value
+            let B24 = document.getElementById("B24").value
+            let B25 = document.getElementById("B25").value
+            let B26 = document.getElementById("B26").value
+            let B27 = document.getElementById("B27").value
+            let B28 = document.getElementById("B28").value
+            let B29 = document.getElementById("B29").value
+            let B30 = document.getElementById("B30").value
+            let B31 = document.getElementById("B31").value
+            let B32 = document.getElementById("B32").value
+            let B33 = document.getElementById("B33").value
+            let B34 = document.getElementById("B34").value
+            let B35 = document.getElementById("B35").value
+            let B36 = document.getElementById("B36").value
+            let B37 = document.getElementById("B37").value
+            let B38 = document.getElementById("B38").value
+            let B39 = document.getElementById("B39").value
+            let B40 = document.getElementById("B40").value
+
+            if (!isNaN(B11) && !isNaN(B12) && !isNaN(B13) && !isNaN(B14) && !isNaN(B15) && !isNaN(B16) && !isNaN(B17) && !isNaN(B18) && !isNaN(B19) && !isNaN(B20)
+            && !isNaN(B21) && !isNaN(B22) && !isNaN(B23) && !isNaN(B24) && !isNaN(B25) && !isNaN(B26) && !isNaN(B27) && !isNaN(B28) && !isNaN(B29) && !isNaN(B30)
+            && !isNaN(B31) && !isNaN(B32) && !isNaN(B33) && !isNaN(B34) && !isNaN(B35) && !isNaN(B36) && !isNaN(B37) && !isNaN(B38) && !isNaN(B39) && !isNaN(B40)
+            ){
+                let resultado = Number(B11) + Number(B12) + Number(B13) + Number(B14) + Number(B15) + Number(B16) + Number(B17) + Number(B18) + Number(B19) + Number(B20)
+                + Number(B21) + Number(B22) + Number(B23) + Number(B24) + Number(B25) + Number(B26) + Number(B27) + Number(B28) + Number(B29) + Number(B30)
+                 + Number(B31) + Number(B32) + Number(B33) + Number(B34) + Number(B35) + Number(B36) + Number(B37) + Number(B38) + Number(B39) + Number(B40)
+                 
+                 alert(resultado)
+                }
+            }
+    
+    function somar3(){
+
+            let C11 = document.getElementById("C11").value
+            let C12 = document.getElementById("C12").value
+            let C13 = document.getElementById("C13").value
+            let C14 = document.getElementById("C14").value
+            let C15 = document.getElementById("C15").value
+            let C16 = document.getElementById("C16").value
+            let C17 = document.getElementById("C17").value
+            let C18 = document.getElementById("C18").value
+            let C19 = document.getElementById("C19").value
+            let C20 = document.getElementById("C20").value
+            let C21 = document.getElementById("C21").value
+            let C22 = document.getElementById("C22").value
+            let C23 = document.getElementById("C23").value
+            let C24 = document.getElementById("C24").value
+            let C25 = document.getElementById("C25").value
+            let C26 = document.getElementById("C26").value
+            let C27 = document.getElementById("C27").value
+            let C28 = document.getElementById("C28").value
+            let C29 = document.getElementById("C29").value
+            let C30 = document.getElementById("C30").value
+            let C31 = document.getElementById("C31").value
+            let C32 = document.getElementById("C32").value
+            let C33 = document.getElementById("C33").value
+            let C34 = document.getElementById("C34").value
+            let C35 = document.getElementById("C35").value
+            let C36 = document.getElementById("C36").value
+            let C37 = document.getElementById("C37").value
+            let C38 = document.getElementById("C38").value
+            let C39 = document.getElementById("C39").value
+            let C40 = document.getElementById("C40").value
+
+            if (!isNaN(C11) && !isNaN(C12) && !isNaN(C13) && !isNaN(C14) && !isNaN(C15) && !isNaN(C16) && !isNaN(C17) && !isNaN(C18) && !isNaN(C19) && !isNaN(C20)
+            && !isNaN(C21) && !isNaN(C22) && !isNaN(C23) && !isNaN(C24) && !isNaN(C25) && !isNaN(C26) && !isNaN(C27) && !isNaN(C28) && !isNaN(C29) && !isNaN(C30)
+            && !isNaN(C31) && !isNaN(C32) && !isNaN(C33) && !isNaN(C34) && !isNaN(C35) && !isNaN(C36) && !isNaN(C37) && !isNaN(C38) && !isNaN(C39) && !isNaN(C40)
+            ){
+                let resultado = Number(C11) + Number(C12) + Number(C13) + Number(C14) + Number(C15) + Number(C16) + Number(C17) + Number(C18) + Number(C19) + Number(C20)
+                + Number(C21) + Number(C22) + Number(C23) + Number(C24) + Number(C25) + Number(C26) + Number(C27) + Number(C28) + Number(C29) + Number(C30)
+                 + Number(C31) + Number(C32) + Number(C33) + Number(C34) + Number(C35) + Number(C36) + Number(C37) + Number(C38) + Number(C39) + Number(C40)
+                 
+                 alert(resultado)
+                }
+             }  
+          
+             
